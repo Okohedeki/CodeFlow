c3e46cf3ec9ad333d45453ada2f43435720a4da0
Edeki Okoh <Okohedeki@gmail.com>
2022-08-24 01:15:51 -0700

    Checking in changes for ctags

--- CodeFlow/scripts/Utils.py
+++ CodeFlow/scripts/Utils.py
@@ -1,9 +1,16 @@
 import inspect
 import json
 from TabNannyTest import check
+import subprocess
+import os
 
 
 if __name__ == '__main__':
-    data = check('/home/okohedeki/Desktop/CodeFlow/SampleProject/backend/app.py')
-    json_object = json.dumps(data, indent = 4) 
-    print(json_object)
+    #data = check('/home/okohedeki/Desktop/CodeFlow/SampleProject/backend/app.py')
+   # Logical_indent_json = json.dumps(data, indent = 4) 
+
+
+    #universal-ctags --output-format=json --fields="*" -R backend/app.py > test.json
+    
+    os.system("universal-ctags --output-format=json --fields=""a+e+E+f+m+i+F+K+Z+l+n+P+r+s+Z+p+S+t"" -R ../../SampleProject/backend/app.py > test.json")
+    #print(Logical_indent_json[0])
